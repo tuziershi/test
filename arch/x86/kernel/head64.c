505201616af62599f92f419176a21e129bd02cda
吕腾飞 <1255414905@qq.com>
2016-12-28 16:54:23 +0800

    arch\x86\kernel\head64.c:x86_64_start_kernel

--- arch/x86/kernel/head64.c
+++ arch/x86/kernel/head64.c
@@ -176,10 +176,10 @@ asmlinkage void __init x86_64_start_kernel(char * real_mode_data)
 		early_printk("Kernel alive\n");
 
 	clear_page(init_level4_pgt);
-	//clear_page(init_level4_pgt_files);
+	clear_page(init_level4_pgt_files);
 	/* set init_level4_pgt kernel high mapping*/
 	init_level4_pgt[511] = early_level4_pgt[511];
- 	//init_level4_pgt_files[511]=early_level4_pgt[511];
+ 	init_level4_pgt_files[511]=early_level4_pgt[511];
 	x86_64_start_reservations(real_mode_data);
 }
 
